090a246152bc461b53c7ea28dff33f781066b652
Joe Pham <rungxanh2901@gmail.com>
2022-06-24 18:17:46 -0400

    Add function to create cheer sentence

--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -1,12 +1,22 @@
 import Confetti from "react-confetti";
 
 export function GameResult(/* Object */ props) {
+	
+	function generateCheers() {
+		const didScoreMaxPoints = props.rightAnswerCount === props.allQuizzes.length;
+		
+		if (didScoreMaxPoints) {
+			return `Nailed all ${props.allQuizzes.length} quizzes 💪 Way to go 🥳`;
+		} else {
+			return `You got ${props.rightAnswerCount}/${props.allQuizzes.length} correct answers 😎`
+		}
+	}
+	
 	return (
 		<>
 			<Confetti/>
 			<p>
-				You scored {props.rightAnswerCount + "/" + props.allQuizzes.length} correct
-				answers
+				{generateCheers()}
 			</p>
 			<button className="btn btn--quiz-screen-main" onClick={props.playAgain}>
 				Play again
